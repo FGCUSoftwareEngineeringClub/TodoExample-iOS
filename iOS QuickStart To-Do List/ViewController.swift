5a27898c67a5b45682c16e0f1af5b8cb1ed710f8
Brandon <bbrandonbaker.b@gmail.com>
2020-02-24 00:01:20 -0500

    Initial Commit

---
+++ iOS QuickStart To-Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS QuickStart To-Do List
+//
+//  Created by Brandon Baker on 2/24/20.
+//  Copyright © 2020 Brandon Baker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
